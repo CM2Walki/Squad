8071b15a4026dd633ef0acc26fc16234b2649c13
d0cplay <docplay.emp@gmail.com>
2021-06-10 10:21:49 +0200

    fixes install dir

--- etc/entry.sh
+++ etc/entry.sh
@@ -7,6 +7,9 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "/home/steam/squad-dedicated/SquadGame/ServerConfig/Rcon.cfg"
 
+bash "mv -R /home/steam/Steam/steamapps/common/Squad Dedicated Server/ /home/steam/squad-dedicated/"
+
+
 bash "/home/steam/squad-dedicated/SquadGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
