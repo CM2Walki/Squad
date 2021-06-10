269f70507bb55570608c763ca1fe0e506397a229
d0cplay <docplay.emp@gmail.com>
2021-06-10 10:03:16 +0200

    fixed steamcmd bug

--- etc/entry.sh
+++ etc/entry.sh
@@ -5,12 +5,13 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+quit
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
-sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
+sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "/home/steam/squad-dedicated/SquadGame/ServerConfig/Rcon.cfg"
 
-bash "${STEAMAPPDIR}/SquadGameServer.sh" \
+bash "/home/steam/squad-dedicated/SquadGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
 			RCONPORT="${RCONPORT}" \
 			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
 			FIXEDMAXTICKRATE="${FIXEDMAXTICKRATE}" \
 			RANDOM="${RANDOM}"
+bash
