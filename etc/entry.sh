ce23720033caffc8950fb9fa3541c73e62da9c0d
d0cplay <docplay.emp@gmail.com>
2021-12-17 11:44:07 +0100

    added beta compatibility

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
-				+force_install_dir "/home/steam/squad-dedicated/" \
-				+app_update "${STEAMAPPID}" \
-				+quit
-
+if ${STEAM_BETA_BRANCH} != ""
+then
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+					+login anonymous \
+					+app_update "${STEAM_BETA_APP}" \
+					+beta "${STEAM_BETA_BRANCH}" \
+					+betapassword "${STEAM_BETA_PASSWORD}" \
+					+quit
+else
+	bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+					+login anonymous \
+					+app_update "${STEAMAPPID}" \
+					+quit
+fi
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
-sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "/home/steam/squad-dedicated/SquadGame/ServerConfig/Rcon.cfg"
+sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
 
-bash "mv -R /home/steam/Steam/steamapps/common/Squad Dedicated Server/ /home/steam/squad-dedicated/"
-
-
-bash "/home/steam/squad-dedicated/SquadGameServer.sh" \
+bash "${STEAMAPPDIR}/SquadGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
 			RCONPORT="${RCONPORT}" \
 			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
 			FIXEDMAXTICKRATE="${FIXEDMAXTICKRATE}" \
 			RANDOM="${RANDOM}"
-bash
