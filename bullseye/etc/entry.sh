4227d0af7333d0c710fd93d3dcc644b1e793e4da
Alex Diller <a.diller@dolphbit.de>
2022-08-16 16:34:22 +0200

    Add mod support & include entry.sh directly

--- bullseye/etc/entry.sh
+++ bullseye/etc/entry.sh
@@ -19,6 +19,27 @@ fi
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
 
+echo "Clearing Mods..."
+# Clear all workshop mods:
+# find all folders / files in mods folder which are numeric only;
+# remove the workshop mods
+find $MODPATH/* -maxdepth 0 -regextype posix-egrep -regex ".*/[[:digit:]]+" | xargs -d"\n" rm -R 2>/dev/null
+
+# Install mods (if defined)
+declare -a MODS="$MODS"
+if (( ${#MODS[@]} ))
+then
+	echo "Installing Mods..."
+	for MODID in ${MODS[@]}; do
+		echo "> Install mod '${MODID}'"
+		"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir $STEAMAPPDIR +login anonymous +workshop_download_item $WORKSHOPID $MODID +quit
+
+		echo -e "\n> Link mod content '${MODID}'"
+		# cp -R "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}" "${MODPATH}/${MODID}" 2>/dev/null
+		ln -s "${STEAMAPPDIR}/steamapps/workshop/content/${WORKSHOPID}/${MODID}" "${MODPATH}/${MODID}"
+	done
+fi
+
 bash "${STEAMAPPDIR}/SquadGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
